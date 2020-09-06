ade031e6ba786f7ed4f2dd9dca3d0f7f5fce70b7
Thor40 <nicholas.kosik@gmail.com>
2020-09-06 13:39:22 -0500

    updating info

--- kosikportfolio/src/components/About.js
+++ kosikportfolio/src/components/About.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
-// import data from '../mydata';
+import data from '../mydata';
 
+import img from '../assets/images/image0.jpeg'
 function About() {
 
     return(
         <div className='about'>
             <div className='about-content'>
-            <h1><Fade bottom cascade>About.</Fade></h1>
+            <h1><Fade bottom cascade>About Me</Fade></h1>
             <Fade bottom>
-                    <p>Hello, it me!</p>
+                    <p>{data.abouttext}</p>
             </Fade>
             </div>
-            {/* {data.ShowAboutImage ? <img src={data.aboutImage} alt='about iamge'></img> : null} */}
+            <img src={img} alt='about image'></img>
         </div>
     );
 }
