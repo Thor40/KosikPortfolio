7d8ae473eacf33946705456ea095bbf7e857c837
Thor40 <nicholas.kosik@gmail.com>
2020-09-08 22:19:05 -0500

    contant finisheD

--- kosikportfolio/src/components/Contact.js
+++ kosikportfolio/src/components/Contact.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
 import data from '../mydata';
+import { FaGithubSquare  } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { GrInstagram } from 'react-icons/gr';
 
 function Contact() {
 
     return( 
         <div>
-            <h1>
+            <h1 className="mt-5 p-2">
                     <Fade bottom cascade> Contact</Fade>
             </h1>
-            <Fade bottom>
-            <div className='contact-content'>
-                    <h1>
-                    Let’s collaborate<br></br></h1>
-                        <a href={`mailto:${data.contactEmail}`} className='email'>{data.contactEmail}</a>
-                    <ul>
-                        {data.social.map((link,index)=>(
-                            <li key={index}><a target='_blank' rel="noopener noreferrer" href={link.url}>{link.name}</a></li>
-                        ))}   
-                    </ul>
-            </div>
-            </Fade>
+            <div className="mt-5 p-2">
+              <a
+                href="https://github.com/Thor40"
+                className="m-3"
+              >
+                <FaGithubSquare size={80} style={{ fill: 'black' }}/>
+              </a>
+              <a href="https://www.linkedin.com/in/nicholas-kosik-17405a1a3/" className="m-3">
+                <FaLinkedin size={80} style={{ fill: 'black' }} />
+              </a>
+              <a
+                href="https://www.instagram.com/thorkosik/"
+                className="m-3"
+              >
+                <GrInstagram size={80} style={{ fill: 'black' }} />
+              </a>
+          </div>  
         </div>
     );
 };
