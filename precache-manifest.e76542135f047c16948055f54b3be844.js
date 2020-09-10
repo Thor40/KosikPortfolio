ee1571584ce9b69287d1a98efe3e2180889ecb09
Thor40 <nicholas.kosik@gmail.com>
2020-09-09 20:52:57 -0500

    Updates

---
+++ precache-manifest.e76542135f047c16948055f54b3be844.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6335bd9e9afc519fb68345580331f0f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b2ad8ac8ea5dd3856b36",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "70295b3f10bd41efe8c4",
+    "url": "./static/css/main.ae2ed7ac.chunk.css"
+  },
+  {
+    "revision": "b2ad8ac8ea5dd3856b36",
+    "url": "./static/js/2.4fb952ae.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.4fb952ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70295b3f10bd41efe8c4",
+    "url": "./static/js/main.01622bb5.chunk.js"
+  },
+  {
+    "revision": "c478984e1bed5b17e757",
+    "url": "./static/js/runtime-main.31deeb73.js"
+  }
+]);
